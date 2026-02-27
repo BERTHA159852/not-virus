7fafa3c9134a0cc4946ef01cf377a5bc1c841d12
BERTHA159852 <160510720+BERTHA159852@users.noreply.github.com>
2026-02-27 14:29:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let language = 'VI';
 
+const startBtn = document.getElementById('start-btn');
+const langBtn = document.getElementById('lang-btn');
+
+// Đổi ngôn ngữ
+langBtn.addEventListener('click', () => {
+  language = language === 'VI' ? 'EN' : 'VI';
+  langBtn.textContent = language;
+
+  startBtn.textContent = language === 'VI' ? 'BẮT ĐẦU' : 'START';
+});
+
+// Bấm bắt đầu → animation
+startBtn.addEventListener('click', () => {
+  startBtn.classList.add('fly-up');
+
+  setTimeout(() => {
+    langBtn.classList.add('fly-up');
+  }, 400);
+
+  setTimeout(() => {
+    document.getElementById('start-screen').style.display = 'none';
+    // 👉 Tại đây bạn chuyển sang màn hình game
+    // showScreen('screen-1');
+  }, 1200);
+});
