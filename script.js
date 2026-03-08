3e32a969c6dfb59719098423cd4114f7c00f0449
BERTHA159852 <160510720+BERTHA159852@users.noreply.github.com>
2026-03-09 00:14:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ startBtn.addEventListener('click', () => {
     document.getElementById('start-screen').style.display = 'none';
   }, 1200);
 });
-const canvas=document.getElementById("intro-canvas")
+const canvas=document.getElementById("intro-canvas");
 canvas.style.display="block"
 
 startIntro()
